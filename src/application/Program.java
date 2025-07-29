3a933cf90af0da9e641274d25fde13cac1c532c3
Nilson Santiago <nilson.santiago19@gmail.com>
2025-07-29 14:14:47 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,11 +28,15 @@ public class Program {
 //		sellerDao.insert(newSeller);
 //		System.out.println("Inserted! new id = " + newSeller.getId());
 		
-		System.out.println("=== Test 5: Seller update ===");
-		Seller seller = sellerDao.findById(1);
-		seller.setName("Martha Wayne");
-		sellerDao.update(seller);
-		System.out.println("Updated! seller has updated");
+//		System.out.println("=== Test 5: Seller update ===");
+//		Seller seller = sellerDao.findById(1);
+//		seller.setName("Martha Wayne");
+//		sellerDao.update(seller);
+//		System.out.println("Updated! seller has updated");
+		
+		System.out.println("=== Test 6: Seller delete ===");
+		sellerDao.deleteById(2);
+		System.out.println("Deleted!");
 
 	}
 
--- src/model/dao/impl/SellerDaoJdbc.java
+++ src/model/dao/impl/SellerDaoJdbc.java
@@ -86,7 +86,21 @@ public class SellerDaoJdbc implements SellerDao {
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		
+		PreparedStatement ps = null;
+		
+		try {
+			
+			ps = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			
+			ps.setInt(1, id);
+			ps.executeUpdate();
+			
+		} catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(ps);
+		}
 
 	}
 
